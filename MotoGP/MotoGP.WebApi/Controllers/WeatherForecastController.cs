ff89104690298336e784b95f6c4269bf1a05d2a0
Miguel Molina Jr <admin@miguelmolinajr.com>
2023-11-10 08:28:31 -0500

    initial add of a web api project

---
+++ MotoGP/MotoGP.WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MotoGP.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+    }
+}
